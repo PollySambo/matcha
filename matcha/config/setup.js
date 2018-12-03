fa5679021fdf92450a70d6e31700114b3b61dee8
Polite SAMBO <psambo@e5r7p8.wethinkcode.co.za>
2018-12-03 16:45:46 +0200

    first level

--- matcha/config/setup.js
+++
@@ -1,9 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "matcha":
-var url = "mongodb://localhost:8080/matcha";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
