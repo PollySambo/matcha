0ac6e11a76e7d0f486f0d190b7041d826fd79935
PollySambo <psambo@student.wethinkcode.co.za>
2018-12-05 15:21:46 +0200

    played around with fronend, failed

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+	include 'login.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" media="screen" href="match.css" />
+</head>
+  <body>
+		
+    <!-- <div class="containe">
+        <nav class="navbar navbar-expand-sm bg navbar fixed-top">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" width="100" height="100" class="d-inline-block align-top" alt="">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item" style="margin-right:30px">
+                <a class="fas fa-user-plus" href="signup.html"> sign-up</a>
+                </li>
+                <li class="nav-item">
+                <a class="fas fa-sign-in-alt" href="login.html"> sign-in</a>
+                </li>
+            </ul>
+		</nav>
+    </div> -->
+
+  
+	<div class="o-wall">
+		<div class="o-wall__claim">
+		  <div class="o-wall__letters-list">
+			<b class="o-wall__letters-list--letter">
+			  W
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  E
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  L
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  C
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  O
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  M
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  E
+			</b>
+		</div>
+		  <div class="o-wall__letters-list">
+			<b class="o-wall__letters-list--letter">
+			  T
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  O
+			</b>
+		  </div>
+		  <div class="o-wall__letters-list">
+			<b class="o-wall__letters-list--letter">
+			  M
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  A
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  T
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  C
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  H
+			</b>
+			<b class="o-wall__letters-list--letter">
+			  A
+			</b>
+			<p>&hearts;❤️
+			</p>
+		  </div>
+		</div>
+	  </div>
+
+	  <footer class="footer container-fluid text-center">
+		<b><p> psambo &copy matcha 2018</p></b>
+	</footer>
+
+
+  </body>
+</html>
