d9d1e37537ec83c5f6bed64f1798fba2955bcdaf
PollySambo <psambo@student.wethinkcode.co.za>
2018-12-12 15:19:20 +0200

    profiles structure done

---
+++ profile.php
@@ -0,0 +1,72 @@
+
+
+<!Doctype html>
+<html lang="en">
+  <head>
+    <title>profile</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+                <script src="https://unpkg.com/typewriter-effect/dist/core.js"></script>
+    <link rel="stylesheet"  type=text/css href="profile.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+
+        <nav class="navbar navbar-expand-sm bg navbar fixed-top">
+                            <a class="navbar-brand" href="#">
+                                <img src="images/logo.png" width="100" height="100" class="d-inline-block align-top" alt="">
+                            </a>
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item" style="margin-right:30px">
+                                <a class="fas fa-home" href="index.php"> home</a>
+                                <!-- </li>
+                                <li class="nav-item" >
+                                    <a class="fas fa-sign-out-alt" href="signup.php"> signup</a>
+                                  </li> -->
+                            </ul>
+                    </nav>
+
+
+      <div class="split left">
+          <div class="centered">
+          <h1>RIGBY</h1></br>
+
+              <img src="profiles/rigby2.png" alt="regular_show">
+              <p>Age : 20</p>
+              <p>Gender : Male</p>
+              <p>Interest : Women</p>
+              <p>some text</p>
+              <div class="bg">
+            <input type="text" placeholder="Bio"/>
+        </div>
+          </div>
+
+        </div>
+
+       <div class="split right">
+          <div class="centered">
+          <h1>Eileen</h1></br>
+              <img src="profiles/Eileen.png" alt="regular_show">
+              <p><b>Age : 20</b></p>
+              <p><b>Gender : Female</b></p>
+              <p><b>Interest : Women</b></p>
+              <div class="bg">
+            <input type="text" placeholder="Bio">
+        </div>
+          </div>
+          </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
