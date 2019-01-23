cd8b1ccc37acf3549dc027c56a6b408dc9fd7635
Polite SAMBO <psambo@e4r17p8.wethinkcode.co.za>
2019-01-23 15:27:39 +0200

    geolocation works

--- php/talkback.php
+++ php/talkback.php
@@ -1,4 +1,6 @@
 <?php
+
+
 print_r($_POST);
 // var_dump($_POST['longitude']);
 // var_dump($_POST['latitude']);
@@ -8,5 +10,6 @@ print_r($_POST);
         echo "<p>my name is not" + $_POST['longitude']. + $_POST['latitude']."</p>";
     }
 
-    
+
+
     // print_r($_SESSION['myname'])    ?>
