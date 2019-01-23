58d00b8990e3ece90ea767d9189aef5b38e974b5
Polite SAMBO <psambo@e4r17p8.wethinkcode.co.za>
2019-01-23 13:19:30 +0200

    can get location coordinates

--- php/talkback.php
+++ php/talkback.php
@@ -1,5 +1,12 @@
 <?php
-    if (isset($_POST['myname']))
+print_r($_POST);
+// var_dump($_POST['longitude']);
+// var_dump($_POST['latitude']);
+
+    if (isset($_POST['longitude']) && isset($_POST['latitude']))
     {
-        echo "<p>my name is not" + $_POST['myname']."</p>";
+        echo "<p>my name is not" + $_POST['longitude']. + $_POST['latitude']."</p>";
     }
+
+    
+    // print_r($_SESSION['myname'])    ?>
